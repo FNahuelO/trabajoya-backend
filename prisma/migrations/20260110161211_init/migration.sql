eb527a87e52844a04e10786839c65f389f6f06b7
Franco Osorio <franconawel@gmail.com>
2026-01-10 16:56:34 -0300

    chore: remove outdated update notification from migration.sql
    - Deleted the major version update notification from the migration.sql file to streamline the migration process and reduce clutter.

--- prisma/migrations/20260110161211_init/migration.sql
+++ prisma/migrations/20260110161211_init/migration.sql
@@ -701,14 +701,3 @@ ALTER TABLE "PaymentTransaction" ADD CONSTRAINT "PaymentTransaction_userId_fkey"
 
 -- AddForeignKey
 ALTER TABLE "PaymentTransaction" ADD CONSTRAINT "PaymentTransaction_empresaId_fkey" FOREIGN KEY ("empresaId") REFERENCES "EmpresaProfile"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
-┌─────────────────────────────────────────────────────────┐
-│  Update available 5.22.0 -> 7.2.0                       │
-│                                                         │
-│  This is a major update - please follow the guide at    │
-│  https://pris.ly/d/major-version-upgrade                │
-│                                                         │
-│  Run the following to update                            │
-│    npm i --save-dev prisma@latest                       │
-│    npm i @prisma/client@latest                          │
-└─────────────────────────────────────────────────────────┘
